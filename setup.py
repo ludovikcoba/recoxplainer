338fa8368914058948d6e69379c977e47f170d1c
Ludovik Coba <CobaLu01@de-ber-m45dml85.fritz.box>
2020-12-14 23:09:02 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='recoexplainer',
+    version='0.0.2',
+    packages=['recoexplainer'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description="""Explainable Recommenders""",
+)
